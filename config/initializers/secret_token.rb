70ed6f84d6e1a723c2ed906d6ccff8f504b87fa6
fukayatsu <fukayatsu@gmail.com>
2013-10-26 11:55:40 +0900

    rails new . -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SimpleUploader::Application.config.secret_key_base = '9e84bbb0bb4db288f34af5ff7c5b25ad75b6fe58d7847c440cf165d2419a125cba436311888f27086f1b012d13f649a9c92e8a681c85b460513b1641cbece8d7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SimpleUploader::Application.config.session_store :cookie_store, key: '_simple-uploader_session'
